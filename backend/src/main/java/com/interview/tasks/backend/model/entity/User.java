11c8f115232d55c1b04d5ead131cd83e0f61d310
jesusdangerous <aleksandrzonov641@gmail.com>
2025-08-31 14:27:53 +0500

    fix: update user entity

--- backend/src/main/java/com/interview/tasks/backend/model/entity/User.java
+++ backend/src/main/java/com/interview/tasks/backend/model/entity/User.java
@@ -2,8 +2,7 @@ package com.interview.tasks.backend.model.entity;
 
 import com.interview.tasks.backend.model.enums.Role;
 import jakarta.persistence.*;
-import lombok.Getter;
-import lombok.Setter;
+import lombok.*;
 
 import java.time.LocalDateTime;
 import java.util.Objects;
@@ -13,6 +12,9 @@ import java.util.UUID;
 @Table(name = "users")
 @Getter
 @Setter
+@AllArgsConstructor
+@NoArgsConstructor
+@Builder
 public class User {
 
     @Id
@@ -40,7 +42,14 @@ public class User {
     }
 
     @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (!(o instanceof User)) return false;
+        return id != null && id.equals(((User) o).getId());
+    }
+
+    @Override
     public int hashCode() {
-        return Objects.hash(id, username, email, password, createdAt, role);
+        return Objects.hash(id);
     }
 }
